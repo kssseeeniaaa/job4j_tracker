5ee29e671868bb619a9489a085cae7cdfd1cee85
kssseeeniaaa <nekakk@mail.ru>
2023-01-26 18:06:04 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #382199]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker;
 
 import java.util.Arrays;
 import java.util.Objects;
-
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -49,24 +48,11 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            items[index] = item;
-            item.setId(id);
+        if (index == -1) {
+            return false;
         }
-        return rsl;
-    }
-
-    public boolean delete(int id) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            items[index] = null;
-            System.arraycopy(items, index + 1,
-                    items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
-        }
-        return rsl;
+        items[index] = item;
+        item.setId(id);
+        return true;
     }
 }
